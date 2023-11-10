3594bd04d28e4571b94896726d52e13eaf18b032
Darin Gregg <daringregg801@gmail.com>
2023-11-09 20:12:20 -0700

    Final project modification. CRUD working

--- routes/products.route.js
+++ routes/products.route.js
@@ -34,10 +34,10 @@ router.post("/", async (req, res, next) => {
 router.put("/:order_id", async (req, res, next) => {
     try {
         const { order_id } = req.params;
-        const updatedProduct = {...req.body, order_id: uuidv4() };
+        const updatedProduct = req.body;
         const data = productsController.updateProduct(order_id, updatedProduct);
         console.log(updatedProduct);
-        res.json(data);
+        //res.json(data);
     } catch(err) {
         next(err);
     }
